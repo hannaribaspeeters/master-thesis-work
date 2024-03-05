9cbdcf9a0c355c919daea1b0286fa0c074816877
jdollinger-bit <72741199+jdollinger-bit@users.noreply.github.com>
2024-03-05 10:16:17 +0100

    Update cross_entropy_loss.py

--- src/criterions/cross_entropy_loss.py
+++ src/criterions/cross_entropy_loss.py
@@ -5,6 +5,7 @@ from src.criterions.abstract_criterion import AbstractCriterion
 
 class CEL(AbstractCriterion):
     def __init__(self):
+        super().__init__()
         self.criterion = torch.nn.CrossEntropyLoss()
 
     def __call__(self, logits, y):
